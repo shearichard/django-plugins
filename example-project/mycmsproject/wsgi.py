0b41c2be8dec97fe1a2972924f27f89dcdd6b53f
Richard Shea <rshea@thecubagroup.com>
2023-10-26 13:54:36 +1300

    Some changes to accomodate the jump to a Django 4.2

--- example-project/mycmsproject/wsgi.py
+++ example-project/mycmsproject/wsgi.py
@@ -4,12 +4,13 @@ WSGI config for mycmsproject project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-from __future__ import absolute_import
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mycmsproject.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycmsproject.settings')
+
 application = get_wsgi_application()
