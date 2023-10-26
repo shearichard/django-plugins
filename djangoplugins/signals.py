79f9126cb1df7ebbbac2fcb4ac1d73524d3fac88
Richard Shea <rshea@thecubagroup.com>
2023-10-26 15:43:42 +1300

    Fix a change to the signature of Signal which has occurred in recent version of Django

--- djangoplugins/signals.py
+++ djangoplugins/signals.py
@@ -1,4 +1,16 @@
 from django.dispatch import Signal
 
-django_plugin_disabled = Signal(providing_args=["plugin"])
-django_plugin_enabled = Signal(providing_args=["plugin"])
+#################################################################
+# Apparently the signature of Signal has changed in >= Django 4.0 
+# so that 'providing_args is no longer needed. Most sources say
+# you can just remove the argument, others suggest you do this
+#
+#  django_plugin_disabled = Signal("plugin")
+#
+# At least initially I'm going with straightforward removal
+#
+#################################################################
+#django_plugin_disabled = Signal(providing_args=["plugin"])
+#django_plugin_enabled = Signal(providing_args=["plugin"])
+django_plugin_disabled = Signal()
+django_plugin_enabled = Signal()
